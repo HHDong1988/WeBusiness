eb45b9b158a052308ca02b5e38d98a119ce5efee
crazyspencer <zibaihezhimao@126.com>
2017-05-22 21:14:25 +0800

    add user type id cookie

--- src/WeProject/src/LogInSystem.java
+++ src/WeProject/src/LogInSystem.java
@@ -79,6 +79,9 @@ public class LogInSystem extends HttpServlet {
 			endDate = new Date();
 			if (array.length()>0) {
 				jObject = HttpUtil.getResponseJson(true, array, endDate.getTime() - beginDate.getTime(), null,0,1,-1);
+				String usertypeid = jObject.getString("UserTypeId");
+				jObject.remove("Password");
+				resp.addCookie(HttpUtil.GetCookie("UserTypeId", usertypeid, 600));
 				resp.addCookie(HttpUtil.GetCookie("username", userName, 600));
 				String tokenstr =  MD5Util.MD5(userName).trim();
 				resp.addCookie(HttpUtil.GetCookie("token", tokenstr, 600));
@@ -96,10 +99,10 @@ public class LogInSystem extends HttpServlet {
 			
 			writer.close();
 			conn.close();
-		} catch (SQLException e) {
+		} catch (SQLException | JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 }
