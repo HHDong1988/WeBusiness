6dc27cd5bdc9e8e32592a951203b99ac4ad72b15
bdong1988 <donghuanhuan1988@hotmail.com>
2017-09-18 21:54:27 +0800

    product delivery

--- src/WeProject/WebContent/js/outOfStoreController.js
+++ src/WeProject/WebContent/js/outOfStoreController.js
@@ -1,83 +1,42 @@
 (function () {
   'use strict';
 
-  angular.module('app-web').controller('outOfStoreController', ['$scope', 'PAGE_SIZE_OPTIONS', outOfStoreController])
+  angular.module('app-web').controller('outOfStoreController', ['$scope', 'PAGE_SIZE_OPTIONS', 'financeService', outOfStoreController])
 
-  function outOfStoreController($scope, PAGE_SIZE_OPTIONS) {
+  function outOfStoreController($scope, PAGE_SIZE_OPTIONS, financeService) {
     var vm = this;
-
-
-    vm.onRefresh = function () {
-      vm.gotoPage(vm.currentPage);
-    }
-
-    vm.onSync = function () {
+    vm.deliverInfo = {ReceiverTel:"",PostNum:""};
+    vm.deliverOrder = function (tel) {
+      vm.deliverInfo.ReceiverTel = tel;
+      $('#confirmDeliverModal').modal();
 
     }
 
+    vm.confirmDeliver = function () {
+      financeService.deliverBill(vm.deliverInfo).then(function (res) {
 
-    vm.refreshPaginator = function () {
+      }, function (error) {
 
-      vm.pages = [];
-      if (vm.dataTotal <= vm.pageSize) {
-        vm.pages.push(1);
-      }
-      else {
-        vm.totalPage = Math.ceil(vm.dataTotal / vm.pageSize);
-        for (var i = 1; i <= vm.totalPage; i++) {
-          vm.pages.push(i);
+      })
+      $('#confirmDeliverModal').modal('hide');
+    }
+
+    vm.getAllPassAuditOrders = function () {
+      financeService.getAllPassAuditBills().then(function (res) {
+        if (res.data.success) {
+          angular.copy(res.data.data, vm.bills);
         }
-      }
-      vm.dataBegin = (vm.currentPage - 1) * vm.pageSize + 1;
-      vm.dataEnd = vm.stocks.length + (vm.currentPage - 1) * vm.pageSize;
+      }, function (error) {
 
-    }
-
-    vm.prevPage = function () {
-      if (vm.currentPage <= 1) {
-        return;
-      }
-
-      vm.currentPage--;
-      vm.gotoPage(vm.currentPage);
-    }
-
-    vm.nextPage = function () {
-      if (vm.currentPage >= vm.totalPage) {
-        return;
-      }
-
-      vm.currentPage++;
-      vm.gotoPage(vm.currentPage);
-    }
-
-    vm.gotoPage = function (page) {
-      if (page < 1) {
-        return;
-      }
-    }
-
-    vm.refreshPage = function () {
-      vm.gotoPage(vm.currentPage);
-    }
-    vm.onQuery = function () {
-
+      })
     }
 
     vm.init = function () {
       vm.language = new LanguageUtility();
-
-      vm.pageSizeOptions = PAGE_SIZE_OPTIONS;
-      vm.pageSize = PAGE_SIZE_OPTIONS[0];
-      vm.pages = [];
-      vm.currentPage = 1;
-      vm.totalPage = 1;
-
-      vm.dataTotal = 0;
-      vm.currentDataCount = 0;
-      vm.dataBegin = 0;
-      vm.dataEnd = 0;
-      vm.currentPageSize = 0;
+      vm.bills = [];
+      vm.cartID = 0;
+      vm.trackingNumber = "";
+      vm.getAllPassAuditOrders();
     };
 
     vm.init();
